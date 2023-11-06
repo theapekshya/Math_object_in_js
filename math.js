8763ce401a40ae5e110c9fd0a87afe903e0a9936
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-11-06 21:11:33 +0545

    Math_object_in_js

---
+++ math.js
@@ -0,0 +1,10 @@
+// Math object 
+
+document.write(Math.PI,"<br>");
+document.write(Math.E,"<br>");
+
+ var x = Math.round(4.7);    
+ document.write(x,"<br>");
+
+ var y = Math.sqrt(4)    //square root
+ document.write(y);
